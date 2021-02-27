7c34ff4dfd2c5dc39df7f97dbab3019ac24bef97
Maciej <mckmusial@gmail.com>
2021-02-28 00:32:26 +0100

    refactoring

--- src/EditEvent.js
+++ src/EditEvent.js
@@ -2,27 +2,27 @@ import React from 'react';
 import "./style.css"
 import validateHour from './validateUtils';
 
-const EditEvent = props =>{
+const EditEvent = ({eventTimeChangeHour, eventTimeChangeMinute,eventTimeChangeSeconds, eventChangeDate, eventChange, eventSubmit}) =>{
     return(
         <div className="flex-container">
             <div className="flex-container__userInput-wrapper"> 
             <div className="flex-container__userInput">
                 <div className="flex-container-inputs__userInput">
-                    <input autoComplete="off" onChange={e => props.eventChange(e)} placeholder="Czynność" type="text" id="actvityName"/>
-                    <input autoComplete="off" onChange={e => props.eventChange(e)} placeholder="Godzina"type="text" id="hour"/>
-                    <input autoComplete="off" onChange={e => props.eventChange(e)} placeholder="Minuta" type="text" id="minute"/>
-                    <input onChange={(e) => props.eventChangeDate()} type="date" id="start" name="trip-start"/>
+                    <input autoComplete="off" onChange={e => eventChangeDate(e)} placeholder="Czynność" type="text" id="actvityName"/>
+                    <input autoComplete="off" onChange={e => eventChange(e)} placeholder="Godzina"type="text" id="hour"/>
+                    <input autoComplete="off" onChange={e => eventChange(e)} placeholder="Minuta" type="text" id="minute"/>
+                    <input onChange={(e) => eventChangeDate()} type="date" id="start" name="trip-start"/>
                 </div>
                 <div className="flex-container__submit">
-                    <button onClick={() => props.eventSubmit()}>ADD</button>
+                    <button onClick={() => eventSubmit()}>ADD</button>
                 </div>
             </div>
             </div>
       
             <div className="display-time__flex-container">
-                <p> Clock - {props.eventTimeChangeHour < 10 ? "0" + props.eventTimeChangeHour : props.eventTimeChangeHour }
-                :{props.eventTimeChangeMinute < 10 ? "0" + props.eventTimeChangeMinute : props.eventTimeChangeMinute}
-                :{props.eventTimeChangeSeconds < 10 ? "0" +props.eventTimeChangeSeconds : props.eventTimeChangeSeconds} </p>
+                <p> Clock - {eventTimeChangeHour < 10 ? "0" + eventTimeChangeHour : eventTimeChangeHour }
+                :{eventTimeChangeMinute < 10 ? "0" + eventTimeChangeMinute : eventTimeChangeMinute}
+                :{eventTimeChangeSeconds < 10 ? "0" +eventTimeChangeSeconds : eventTimeChangeSeconds} </p>
             </div>
         
         </div>
